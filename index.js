61d1d1a99f087e00ded98bd381bdf5550ed66a15
Rifat <mrarifat21@gmail.com>
2025-07-13 11:52:23 +0600

    add user to the database

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // server.js
-const express = require('express');
-const cors = require('cors');
-
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -9,9 +10,53 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db = client.db("bashabari");
+    const usersCollection = db.collection("users");
+
+    // add user to db
+    app.post("/users", async (req, res) => {
+      const email = req.body.email;
+
+      // Check if user already exists
+      const userExists = await usersCollection.findOne({ email });
+      if (userExists) {
+        return res
+          .status(200)
+          .send({ message: "User already exists", inserted: false });
+      }
+
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send({ message: "User added", inserted: true, result });
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 // Basic route
-app.get('/', (req, res) => {
-  res.send('Bashabari backend is running');
+app.get("/", (req, res) => {
+  res.send("Bashabari backend is running");
 });
 
 // Start server
